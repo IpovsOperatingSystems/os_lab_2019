64148ee6ea4bc3b207f2999551339a7764bbca66
itu-dev-1 <fedor.glazunov@bk.ru>
2020-04-10 22:03:04 +0000

    back

--- lab3/src/utils.c
+++ lab3/src/utils.c
@@ -6,6 +6,6 @@ void GenerateArray(int *array, unsigned int array_size, unsigned int seed) {
   srand(seed);
   int i;
   for (i = 0; i < array_size; i++) {
-    array[i] = rand()%10;
+    array[i] = rand();
   }
 }
