ae4cfaa9beb0c594d622891a243fea35c2eb716b
tepminamop <fanbeyblade2001@gmail.com>
2021-02-14 08:03:56 +0000

    second ready

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <string.h>
 #include "revert_string.h"
 
 void RevertString(char *str)
 {
-	// your code here
+	int i = 0;
+	int len = strlen(str);
+	while (i <= strlen(str) / 2 - 1)
+	{
+		char tmp = *(str + i);
+		*(str + i) = *(str + len -1 - i);
+		*(str + len -1 - i) = tmp;
+		i++;
+	}
 }
 
