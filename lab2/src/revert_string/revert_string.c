527e3c29a2a3fc831466520a7357668207ac0d84
st-a-novoseltcev <73393494+st-a-novoseltcev@users.noreply.github.com>
2021-02-25 01:11:29 +0300

    endpoint: end task 2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,25 @@
 #include "revert_string.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	int length = strlen(str) + 1;
+	
+	char* cur = str;
+	char* tmp = malloc(sizeof(char) * length);
+	
+	strcpy(cur, str);
+	
+	tmp += length - 1;
+	*tmp = '\0';
+	do {
+		* --tmp = *cur;
+	}	
+	while (* ++cur != '\0');
+	
+	strcpy(str, tmp);
+	free(tmp);
 }
 
