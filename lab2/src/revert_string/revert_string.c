204332e070e07a40bec08f4f484d0530689743fd
kosnatantinbesp08 <105940623+konstantinbesp08@users.noreply.github.com>
2022-05-21 08:16:44 +0000

    lab2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,19 @@
 
 void RevertString(char *str)
 {
-	// your code here
+	int len = 0;
+  char* tmp_str = str;
+  while (*tmp_str != '\0')
+  {
+    len++;
+    tmp_str++;
+  }
+  
+  tmp_str = 0;
+  for(int i = 0; i < (len/2); i++)
+  {
+    char tmp = *(str + i);
+    *(str + i) = *(str + len - i - 1);
+    *(str + len - i - 1) = tmp;
+  }
 }
-
