3bb10a5bae5bb335c7dabdc24291d976e8b361e5
Vladislav Kurochkin <94047003+kuroochkin@users.noreply.github.com>
2023-10-19 16:52:50 +0000

    Первое задание

--- lab3/src/sequential_min_max.c
+++ lab3/src/sequential_min_max.c
@@ -10,21 +10,30 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  //преобразует аргумент seed в целое число и проверяет, что оно положительно.
   int seed = atoi(argv[1]);
   if (seed <= 0) {
     printf("seed is a positive number\n");
     return 1;
   }
 
+  //Преобразует аргумент arraysize в целое число и проверяет, что оно положительно.
   int array_size = atoi(argv[2]);
   if (array_size <= 0) {
     printf("array_size is a positive number\n");
     return 1;
   }
 
+  //Выделяет память для массива array размером arraysize элементов с
   int *array = malloc(array_size * sizeof(int));
+
+  //Генерирует массив с помощью функции GenerateArray, используя переданный seed.
   GenerateArray(array, array_size, seed);
+
+  //Вызывает функцию GetMinMax для нахождения минимального и максимального элементов
   struct MinMax min_max = GetMinMax(array, 0, array_size);
+
+  //Освобождает память, выделенную для массива array.
   free(array);
 
   printf("min: %d\n", min_max.min);
