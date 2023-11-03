74e743e1c5bb86113a0bbc02db7c3f25f80923f2
Vladislav Kurochkin <94047003+kuroochkin@users.noreply.github.com>
2023-11-03 13:49:40 +0000

    Complete lab6

--- lab5/src/factorial.c
+++ lab5/src/factorial.c
@@ -1,113 +1,153 @@
-#include <stdint.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/time.h>
-#include <pthread.h>
 #include <getopt.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <semaphore.h>
 
-struct somedata
-{
-	uint64_t* a;
-	uint64_t s, e;
-	uint64_t m;
+struct FactArgs {
+    int begin;
+    int end;
 };
 
-pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER; //инициализация mutex
-uint64_t shared;
+int factor = 1;
+int modd = 0;
+//pthread_mutex_t mutx = PTHREAD_MUTEX_INITIALIZER;
+sem_t semaphore;
 
-void *ThreadSum(void *args) {
-    struct somedata *smd = (struct somedata *)args;
-    for(uint64_t i = smd->s; i < smd->e; i++){
-    	pthread_mutex_lock(&mtx); //перехват mutex
-    	*(smd->a) = (*(smd->a)*i)%smd->m;
-    	pthread_mutex_unlock(&mtx); //освобождение mutex
+void CountPart(void *args) {
+    //pthread_mutex_lock(&mutx);
+    sem_wait(&semaphore);
+    struct FactArgs *arrs = (struct FactArgs *)args;
+    int num = 1;
+    for(int i = arrs->begin; i < arrs->end; i++)
+    {
+        num = num%modd * i%modd;
     }
-    return 0;
+    factor = factor % modd * num % modd;
+    //pthread_mutex_unlock(&mutx);
+    sem_post(&semaphore);
 }
 
-int main(int argc, char **argv) {
-    shared = 1;
-    uint32_t k = -1;
-    uint32_t pnum = -1;
-    uint32_t mod = -1;
-    static struct option options[] = {{"k", required_argument, 0, 0},
-                                    {"pnum", required_argument, 0, 0},
-                                    {"mod", required_argument, 0, 0},
-                                    {0, 0, 0, 0}};
 
-    int option_index = 0;
-	while(1)
-	{
-	    int c = getopt_long(argc, argv, "f", options, &option_index);
-		if (c == -1) break;
-		switch(c)
-		{
-		case 0:
-			switch(option_index)
-			{
-				case 0:
-					k = atoi(optarg);
-					if(k < 0)
-						printf("k is a positive number!\n");
-					break;
-				case 1:
-					pnum = atoi(optarg);
-					if(pnum < 1)
-						printf("pnum is a positive number!\n");
-					break;
-				case 2:
-					mod = atoi(optarg);
-					if(mod < 1)
-						printf("mod is a positive number!\n");
-					break;
-			}
-			break;
-		default:
-			printf("Index %d is out of options\n", option_index);
-		}
-	}
-        if (optind < argc) 
-        {
-                printf("Has at least one no option argument\n");
-                return 1;
-        }
-        if (k == -1 || pnum == -1 || mod == -1) 
-        {
-                printf("Usage: %s --k \"num\" --pnum \"num\" --mod \"num\" \n", argv[0]);
-                return 1;
-        }
-    pthread_t threads[pnum]; //создаем потоки
-	struct somedata smd[pnum];
-	for(uint64_t i = 0; i < pnum; i++)
-	{
-		smd[i].a = &shared;
-		smd[i].s = i*k/pnum + 1;
-		smd[i].e = (i == (pnum - 1)) ? k + 1 : (i+1)*k/pnum + 1;
-		smd[i].m = mod;
-	}
+int main(int argc, char **argv)
+{
+    int k = 0;
+    int pnum = 0;
 
-    struct timeval start_time;
-    gettimeofday(&start_time, NULL);
-    
-    for (uint32_t i = 0; i < pnum; i++) {
-        if (pthread_create(&threads[i], NULL, ThreadSum, (void *)&smd[i])) {
-            printf("Error: pthread_create failed!\n");
+    sem_init(&semaphore, 0, 1);//semaphore
+
+    int opt = getopt(argc,argv, "k:");
+    if (opt != 'k')
+    {
+        printf("Usage: %s -k \"num\" --mod \"num\" --pnum \"num\" \n", argv[0]);
+        return 1;
+    }
+    else
+    {
+        k = atoi(optarg);
+        if (k <= 0)
+        {
+            printf("k is a positive number\n");
             return 1;
         }
     }
-    
-    for (uint32_t i = 0; i < pnum; i++) {
-        pthread_join(threads[i], NULL);
+
+    while (true) {
+        int current_optind = optind ? optind : 1;
+
+        static struct option options[] = {{"mod", required_argument, 0, 0},
+                                        {"pnum", required_argument, 0, 0},
+                                        {0, 0, 0, 0}};
+
+        int option_index = 0;
+        int c = getopt_long(argc, argv, "f", options, &option_index);
+
+        if (c == -1) break;
+
+        switch (c) 
+        {
+        case 0:
+            switch (option_index) 
+            {
+            case 0:
+                modd = atoi(optarg);
+                if (modd <= 0) 
+                {
+                    printf("mod is a positive number\n");
+                    return 1;
+                }
+                break;
+            case 1:
+                pnum = atoi(optarg);
+                if (pnum <= 0) 
+                {
+                    printf("pnum is a positive number\n");
+                    return 1;
+                }
+                if (pnum > k)
+                {
+                    printf("pnum must be less or equal to k\n");
+                    pnum = k;
+                    printf("pnum is equal to k now\n");
+                }
+                break;
+
+            defalut:
+                printf("Index %d is out of options\n", option_index);
+            }
+            break;
+
+      case '?':
+        break;
+
+      default:
+        printf("getopt returned character code 0%o?\n", c);
     }
-    
-    struct timeval finish_time;
-    gettimeofday(&finish_time, NULL);
-    
-    double elapsed_time = (finish_time.tv_sec - start_time.tv_sec) * 1000.0;
-    elapsed_time += (finish_time.tv_usec - start_time.tv_usec) / 1000.0;
-    
-    
-    printf("Total: %d\n", shared);
-    printf("Elapsed time: %fms\n", elapsed_time);
+  }
+
+  if (optind < argc) 
+  {
+    printf("Has at least one no option argument\n");
+    return 1;
+  }
+
+  if (modd == 0 || pnum == 0) 
+  {
+    printf("Usage: %s -k \"num\" --mod \"num\" --pnum \"num\" \n", argv[0]);
+    return 1;
+  }
+
+  int ars = k/pnum;
+  int left = k%pnum;
+  struct FactArgs args[pnum];
+  for(int i = 0; i < pnum; i++)
+  {
+      args[i].begin = ars*i + (left < i ? left : i) + 1;
+      args[i].end = ars*(i+1) + (left < i+1 ? left : i+1) + 1;
+  }
+
+  pthread_t threads[pnum];
+  for(int i = 0; i < pnum; i++)
+  {
+      if(pthread_create(&threads[i], NULL, (void*)CountPart, (void *)&(args[i])) != 0)//no problem
+      {
+          printf("pthread error\n");
+          return 1;
+      }
+  }
+
+  for(int i = 0; i < pnum; i++)
+  {
+      pthread_join(threads[i], NULL);
+  }
+
+sem_destroy(&semaphore);// delete
+
+  factor %= modd;
+  printf("Factorial %d by mod %d = %d\n", k, modd, factor);
+
+
     return 0;
 }
